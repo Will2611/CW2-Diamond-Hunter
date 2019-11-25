18ac3df6ce5fed70ac1027d7e9d68517f736aa1e
Will2611 <william.ongjiajiang@gmail.com>
2019-11-25 13:08:19 +0800

    initial commit

---
+++ Map-Viewer/src/module-info.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+/**
+ * @author Default User
+ *
+ */
+module MapViewer {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.swt;
+	requires javafx.web;
+	exports application;
+}
