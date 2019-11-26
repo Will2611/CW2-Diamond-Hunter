ff9f8c9f30b4d61fb109d3e5d0f7a70820490707
Will2611 <william.ongjiajiang@gmail.com>
2019-11-26 14:46:16 +0800

    Make MViewr javaFX project

--- Map-Viewer/src/module-info.java
+++ Map-Viewer/src/module-info.java
@@ -5,14 +5,9 @@
  * @author Default User
  *
  */
-module MapViewer {
+module MapViewerMain {
+	exports MapViewerMain;
+
 	requires javafx.base;
-	requires javafx.controls;
-	requires javafx.fxml;
 	requires javafx.graphics;
-	requires javafx.media;
-	requires javafx.swing;
-	requires javafx.swt;
-	requires javafx.web;
-	exports application;
 }
