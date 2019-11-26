a47b73f9c38a24d4172be21f4b3407c2cfda508f
Will2611 <william.ongjiajiang@gmail.com>
2019-11-26 15:13:31 +0800

    module-info MV update

--- Map-Viewer/src/module-info.java
+++ Map-Viewer/src/module-info.java
@@ -10,4 +10,11 @@ module MapViewerMain {
 
 	requires javafx.base;
 	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.swt;
+	requires javafx.web;
+	
 }
